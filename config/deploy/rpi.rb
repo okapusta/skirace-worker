7d440d8d1bbfa5d7262dbbdb7afac4526853dd07
okapusta <kapuh@ubuntu.com>
2014-03-30 08:54:41 +0200

    after deploy

--- config/deploy/rpi.rb
+++ config/deploy/rpi.rb
@@ -1,3 +1,4 @@
 role :app, 'app@192.168.1.149'
 
-after :deploy, "deploy:build", "deploy:install"#, "deploy:start_god"
+after :deploy, "deploy:build"
+after :deploy, "deploy:install"#, "deploy:start_god"
