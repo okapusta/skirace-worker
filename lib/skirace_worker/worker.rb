e5ddea220b3866ac7dfc7830a7937c037a987226
okapusta <kapuh@ubuntu.com>
2014-03-30 09:03:42 +0200

    faster loop

--- lib/skirace_worker/worker.rb
+++ lib/skirace_worker/worker.rb
@@ -4,7 +4,7 @@ class Worker
   def run
     while true
       capacitor.discharge(options.gpio_pin)      
-      sleep 0.1; reading = 0;
+      sleep 0.01; reading = 0;
 
       gpio.mode(options.gpio_pin, INPUT)
       while gpio.read(options.gpio_pin) == LOW
