0a08267b19bda54868231630431cb780737c0d6d
okapusta <kapuh@ubuntu.com>
2014-05-03 12:01:33 +0200

    fix

--- lib/skirace_worker/formatter.rb
+++ lib/skirace_worker/formatter.rb
@@ -1,7 +1,7 @@
 class Formatter
   def time_format(seconds)
     '%d m, %d s, %f ms ' %
-    [1, 60].inject([seconds]) do |result, unit|
+    [0.1, 60, 60].inject([seconds]) do |result, unit|
       result[0,0] = result.shift.divmod(unit)
       result
     end
